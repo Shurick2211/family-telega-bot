e0d2d8cac07b56ed5332a91acd597f2bc243b6e3
shurick <shurick2211@gmail.com>
2026-08-08 10:14:53 +0300

    commit: termux function + disabled /photo ++

--- src/main/java/org/nimko/com/api/HelloController.java
+++ src/main/java/org/nimko/com/api/HelloController.java
@@ -13,7 +13,8 @@ public class HelloController {
   private static final String PAGE = "static/hello.html";
   private static final String CV_PAGE = "static/cv.html";
   private static final String CV_NATALIA_PAGE = "static/resume_natalia.html";
-  private static final String CV_SOPHIA_PAGE = "static/resume_sophia.html";
+  private static final String CV_SOPHIA_PAGE = "static/resume_sophiya.html";
+  private static final String CV_OLEKSANDR_ML_PAGE = "static/cv_oleksandr_nimko_ml.html";
 
   @GetMapping(value = "/hello", produces = MediaType.TEXT_HTML_VALUE)
   public String hello() throws IOException {
@@ -36,6 +37,12 @@ public class HelloController {
   @GetMapping(value = "/sophiya_cv", produces = MediaType.TEXT_HTML_VALUE)
   public String sophiya_cv() throws IOException {
     return new ClassPathResource(CV_SOPHIA_PAGE)
+        .getContentAsString(StandardCharsets.UTF_8);
+  }
+
+  @GetMapping(value = "/oleksandr_nimko_ml_cv", produces = MediaType.TEXT_HTML_VALUE)
+  public String oleksandr_nimko_ml_cv() throws IOException {
+    return new ClassPathResource(CV_OLEKSANDR_ML_PAGE)
         .getContentAsString(StandardCharsets.UTF_8);
   }
 }
